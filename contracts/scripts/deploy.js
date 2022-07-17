0121ce4579e46ee3b78d9601ae36be3d784192b5
pisuthd <pisuth.dae@gmail.com>
2022-07-17 17:41:35 +0900

    feat: update deploy scripts

--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -7,18 +7,23 @@
 const hre = require("hardhat");
 
 async function main() {
-  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  // const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  // const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  
+  const chainId = 137; // change here
 
-  // const lockedAmount = hre.ethers.utils.parseEther("1");
+  const Marketplace = await hre.ethers.getContractFactory("Marketplace")
 
-  // const Lock = await hre.ethers.getContractFactory("Lock");
-  // const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const marketplace = await Marketplace.deploy(chainId);
 
-  // await lock.deployed();
+  await marketplace.deployed();
 
-  // console.log("Lock with 1 ETH deployed to:", lock.address);
+  console.log("marketplace deployed to:", marketplace.address);
+
+  await hre.run("verify:verify", {
+    address: marketplace.address,
+    constructorArguments: [
+      chainId
+    ],
+  });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
